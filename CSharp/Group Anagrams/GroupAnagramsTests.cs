0ab81bf57628e9cce25dfadb91f87a41813bd9c0
Darren Foley <6538994+darrenfoley@users.noreply.github.com>
2024-06-14 13:47:39 -0600

    Add C# solution for Group Anagrams

---
+++ CSharp/Group Anagrams/GroupAnagramsTests.cs
@@ -0,0 +1,60 @@
+﻿namespace CSharp.Group_Anagrams
+{
+    public class GroupAnagramsTests
+    {
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> countsToGroups = [];
+
+            foreach (string str in strs)
+            {
+                char[] counts = new char[26];
+                foreach (char ch in str)
+                {
+                    ++counts[ch - 'a'];
+                }
+                string key = new(counts);
+                if (!countsToGroups.TryGetValue(key, out IList<string>? group))
+                {
+                    group = new List<string>();
+                    countsToGroups[key] = group;
+                }
+                group.Add(str);
+            }
+
+            return countsToGroups.Values.ToList();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            IList<IList<string>> expected = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]];
+            IList<IList<string>> actual = GroupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
+            Assert.Equivalent(expected, actual, true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            IList<IList<string>> expected = [[""]];
+            IList<IList<string>> actual = GroupAnagrams([""]);
+            Assert.Equivalent(expected, actual, true);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            IList<IList<string>> expected = [["a"]];
+            IList<IList<string>> actual = GroupAnagrams(["a"]);
+            Assert.Equivalent(expected, actual, true);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            IList<IList<string>> expected = [];
+            IList<IList<string>> actual = GroupAnagrams([]);
+            Assert.Equivalent(expected, actual, true);
+        }
+    }
+}
---
+++ CSharp/Group Anagrams/README.md
@@ -0,0 +1,12 @@
+# Group Anagrams
+https://leetcode.com/problems/group-anagrams/
+
+## Strategy
+Generate a key for each input string based on its character counts, grouping the strings with the same key together using a `Dictionary` from key to anagram group.
+
+## Complexity
+#### Time: O(mn)
+We have an outer loop iterating over the input strings and an inner loop iterating over the characters in each string, so we have O(mn) where m is the number of input strings and n is the length of the longest string.
+
+#### Space: O(mn)
+We have a `Dictionary` to hold the mappings from counts to anagram groups. In the worst case each group will contain only one string so the space complexity will be O(m(n + 26)), where m is the number of input strings, n is the length of the longest string, and 26 is the size of each of the count strings used as keys. This simplifies to O(mn).
